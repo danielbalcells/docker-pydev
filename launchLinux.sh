c89a226be554e9d000ab01d85622ac1a2f206c99
danielbalcells <dbalcells@gmail.com>
2016-04-26 21:57:31 +0200

    Using new image name

--- launchLinux.sh
+++ launchLinux.sh
@@ -5,6 +5,6 @@ if [[ -z $1 ]]; then codedir=/home/daniel/code; else codedir=$1; fi
 if [[ -z $2 ]]; then containername=pydev; else containername=$2; fi
 
 
-cmd="docker run -it --detach --net=host --name=$containername -P -p 80:80 -u daniel:daniel -v $codedir:/home/daniel/code danielbalcells/devel-python"
+cmd="docker run -it --detach --net=host --name=$containername -P -p 80:80 -u daniel:daniel -v $codedir:/home/daniel/code danielbalcells/pydev:latest"
 echo $cmd
 $cmd
--- launchOSX.sh
+++ launchOSX.sh
@@ -5,6 +5,6 @@ if [[ -z $1 ]]; then codedir=/Users/daniel/code; else codedir=$1; fi
 if [[ -z $2 ]]; then containername=pydev; else containername=$2; fi
 
 
-cmd="docker run --memory=4g -it --detach --net=host --name=$containername -P -p 80:80 -u daniel:daniel -v $codedir:/home/daniel/code danielbalcells/devel-python"
+cmd="docker run --memory=4g -it --detach --net=host --name=$containername -P -p 80:80 -u daniel:daniel -v $codedir:/home/daniel/code danielbalcells/pydev:latest"
 echo $cmd
 $cmd
