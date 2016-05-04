ca809a2ed8d44dd29b8f4d4096a886c4711a4907
danielbalcells <dbalcells@gmail.com>
2016-05-04 15:49:04 +0200

    Enabling CUDA support, preliminary version

---
+++ launchGPULinux.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+# Usage: ./launch.sh code_dir container_name
+DEFAULT_GPU_OPTS="--device /dev/nvidia0:/dev/nvidia0 \
+        --device /dev/nvidia1:/dev/nvidia1 \
+        --device /dev/nvidiactl:/dev/nvidiactl \
+        --device /dev/nvidia-uvm:/dev/nvidia-uvm"
+if [[ -z $1 ]]; then codedir=/home/daniel/code; else codedir=$1; fi
+if [[ -z $2 ]]; then containername=pydev; else containername=$2; fi
+if [[ -z $3 ]]; then
+        gpuopts=$DEFAULT_GPU_OPTS;
+else
+        gpuopts=$(cat $3);
+fi
+
+cmd="docker run -it --detach --net=host --name=$containername -P -p 80:80 \
+        -u daniel:daniel -v $codedir:/home/daniel/code \
+        $gpuopts \
+        danielbalcells/pydev:latest"
+echo $cmd
+$cmd
