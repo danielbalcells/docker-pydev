173320d5d850a40a7171657d7274571800092887
danielbalcells <dbalcells@gmail.com>
2016-05-07 16:08:41 +0200

    fuera kk

--- osxLaunch.sh
+++ osxLaunch.sh
@@ -8,4 +8,3 @@ if [[ -z $2 ]]; then containername=pydev; else containername=$2; fi
 cmd="docker run --memory=4g -it --detach --net=host --name=$containername -P -p 80:80 -u daniel:daniel -v $codedir:/home/daniel/code danielbalcells/pydev:latest"
 echo $cmd
 $cmd
-kk
